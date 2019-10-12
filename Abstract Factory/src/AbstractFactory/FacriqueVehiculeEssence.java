79ab9ea9b83205ca6f212028e6b1c776c46c02da
Tarek <Tarek@DESKTOP-SMEHBBR>
2019-10-12 01:00:08 -0800

    First Commit

---
+++ Abstract Factory/src/AbstractFactory/FacriqueVehiculeEssence.java
@@ -0,0 +1,14 @@
+package AbstractFactory;
+
+public class FacriqueVehiculeEssence implements FabriqueVehicule {
+	
+	public Automobile creeAutomobile(String modele, String couleur, int puissance, double espace)
+		  {
+		    return new AutomobileEssence(modele, couleur, puissance, espace);
+		  }
+
+	public Scooter creeScooter(String modele, String couleur, int puissance)
+		  {
+		    return new ScooterEssence(modele, couleur, puissance);
+		  }
+}
